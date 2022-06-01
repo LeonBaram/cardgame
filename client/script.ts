b471de29db626a61061c2be850d87c3a41231214
LeonBaram <leonbaram@protonmail.com>
2022-06-01 17:27:49 -0400

    Implement basic Phaser.Game init stub

--- client/script.ts
+++ client/script.ts
@@ -1,5 +1,20 @@
+import Phaser from "phaser";
+
 const socket = new WebSocket("ws://localhost:3000");
 
 socket.onopen = (event) => {
   socket.send("conk");
 };
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  parent: "game-area",
+  width: 1920,
+  height: 1080,
+  scene: {
+    preload: function () {
+      this.load.atlas("card", "assets/card.jpg");
+    },
+    create: function () {},
+  },
+});
