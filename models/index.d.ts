ba4936938c4ebf7abd2020d5c38d4dbc9ed506f4
LeonBaram <leonbaram@protonmail.com>
2022-06-05 06:20:40 -0400

    Remove GameObjectType from models/index exports

--- models/index.d.ts
+++ models/index.d.ts
@@ -1,3 +1,3 @@
 export type { EventTypes, EventHandlers, WebSocketEventData } from "./Events";
-export type { GameObjectType, Card, Deck, Counter } from "./GameObjects";
+export type { Card, Deck, Counter } from "./GameObjects";
 export type { ScryfallCardData } from "./ScryfallCardData";
