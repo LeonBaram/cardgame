3799d0a994befc3ccf0a1f9fa57a9399191c70fe
LeonBaram <leonbaram@protonmail.com>
2022-06-05 05:40:27 -0400

    Create stub for server-side event handler funcs

---
+++ server/event-handlers.ts
@@ -0,0 +1,43 @@
+import type { EventHandlers } from "../models/Events";
+
+export const eventHandlers: EventHandlers = {
+  PlayerJoined: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  PlayerLeft: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  PlayerBecameHost: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectCreated: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectDeleted: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectMoved: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectRotated: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectFlipped: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  GameObjectCopied: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  DeckInsertedCard: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  DeckRemovedCard: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  DeckReordered: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+  CounterUpdated: ({ ...args }): void => {
+    throw new Error("Function not implemented.");
+  },
+};
