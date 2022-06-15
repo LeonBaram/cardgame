4db213f74f8156d7ee68343cfe1a8ccc299e18da
LeonBaram <leonbaram@protonmail.com>
2022-06-15 03:22:03 -0400

    Create new GameObject types and refactor existing
    - rename GameObjectType to GameObjectTypes
    - introduce separate namespaces "Client" and "Server" for game object
      types, to segregate client- and server-side game object
      representation.
    - implement the following types under Client and Server:
      - CommonGameObject: properties common to all game objects
      - GameObjectData: the different types of data/underlying state game
        objects may have
      - AnyGameObjectData: a union of all possible types of GameObjectData
      - GameObjects: the different types of possible game object types,
        defined as a union of CommonGameObject properties plus all
        properties unique to that specific game object type
      - AnyGameObject: a union of all possible types of GameObjects
    
    Note - as of this commit, the difference in game object representation
    between the server and client is that the client uses a detailed,
    "heavy" representation, both for game objects' physical attributes
    (Phaser library class) and for underlying state (Scryfall API
    interface). The server instead uses minimal, basic properties (e.g.
    isFaceUp, angle, x, y, scryfallID).

--- models/GameObjects.d.ts
+++ models/GameObjects.d.ts
@@ -1,21 +1,48 @@
 import type * as Phaser from "phaser";
 import type { ScryfallCardData } from "./ScryfallCardData";
 
-export type GameObjectType = "Card" | "Deck" | "Counter";
+export type GameObjectTypes = "Card" | "Deck" | "Counter";
 
-type GameObject = {
-  readonly sprite: Phaser.GameObjects.Image;
-  readonly data: any;
-};
+export namespace Client {
+  export type CommonGameObject = {
+    sprite: Phaser.GameObjects.Image;
+  };
 
-export type Card = GameObject & {
-  readonly data: ScryfallCardData;
-};
+  export type GameObjectData = {
+    Card: { data: ScryfallCardData };
+    Deck: { data: ScryfallCardData[] };
+    Counter: { val: number };
+  };
 
-export type Deck = GameObject & {
-  readonly data: ScryfallCardData[];
-};
+  export type AnyGameObjectData = GameObjectData[GameObjectTypes];
 
-export type Counter = GameObject & {
-  readonly data: number;
-};
+  export type GameObjects = {
+    [T in GameObjectTypes]: CommonGameObject & GameObjectData[T];
+  };
+
+  export type AnyGameObject = GameObjects[GameObjectTypes];
+}
+
+export namespace Server {
+  export type CommonGameObject = {
+    gameObjectType: GameObjectTypes;
+    isFaceUp: boolean;
+    angle: number;
+    x: number;
+    y: number;
+  };
+
+  export type GameObjectData = {
+    Card: { scryfallID: string };
+    Deck: { scryfallIDs: string[] };
+    Counter: { val: number };
+  };
+
+  export type AnyGameObjectData = GameObjectData[GameObjectTypes];
+
+  export type GameObjects = {
+    [T in GameObjectTypes]: CommonGameObject & GameObjectData[T];
+  };
+
+  export type AnyGameObject = GameObjects[GameObjectTypes];
+}
