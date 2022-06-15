3307610f1f3ce4d9b96c1af5a3e3b1858da7d459
LeonBaram <leonbaram@protonmail.com>
2022-06-15 03:48:42 -0400

    Update models/index

--- models/index.d.ts
+++ models/index.d.ts
@@ -1,5 +1,14 @@
-export type { EventTypes, EventHandlers, WebSocketEventData } from "./Events";
-export type { Card, Deck, Counter } from "./GameObjects";
+export type {
+  AnyEvent,
+  AppEvents,
+  CommonEvent,
+  EventHandler,
+  EventHandlerContext,
+  EventHandlerTable,
+  GameObjectEvent,
+  PlayerEvent,
+} from "./Events";
+export type { Client, Server, GameObjectTypes } from "./GameObjects";
 export type { ScryfallCardData } from "./ScryfallCardData";
 export type { Player } from "./Player";
 export type { Room } from "./Room";
