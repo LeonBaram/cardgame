62752e2f4bd51eed127ef9d290d5bfa99f95a6f4
LeonBaram <leonbaram@protonmail.com>
2022-06-15 03:51:41 -0400

    Implement serverside event handlers and middleware
    In event handlers:
    - implement handler for each event under AppEvents
    - extract common functionality and wrap around each event handler
      - common input validation (mostly for ID keys, e.g. for a room,
        player, and/or game object)
      - event broadcasting
    
    In main server file:
    - add stub for new static route
    - rename "wss" to "socketServer" and enable client tracking
    - add Maps for tracking current players and rooms
    - import and make use of event handlers (see above)
    - add roomID parsing via websocket request url param
    - manually trigger PlayerJoined and PlayerLeft events on websocket
      events 'connection' and 'close' respectively

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,50 @@
+import { randomUUID } from "crypto";
 import * as express from "express";
 import { createServer } from "http";
 import { Server as WebSocketServer } from "ws";
+import { createEventHandlers } from "./event-handlers";
 
+import type { IncomingMessage } from "http";
 import type { WebSocket } from "ws";
-import type { WebSocketEventData } from "../models/Events";
+import type { AnyEvent, EventHandler, Player, Room } from "../models";
 
 const app = express();
-
-app.use(express.static("dist/client"));
+app
+  .use("/", express.static("dist/client"))
+  .use("/rooms", express.static("dist/client/rooms"));
 
 const httpServer = createServer(app);
 
-const wss = new WebSocketServer({ server: httpServer });
+const socketServer = new WebSocketServer({
+  server: httpServer,
+  clientTracking: true,
+});
 
-wss.on("connection", (ws: WebSocket) => {
-  ws.send("[immediate greeting]");
+const players = new Map<string, Player>();
+const rooms = new Map<string, Room>();
+const eventHandlers = createEventHandlers({ rooms, players, socketServer });
 
-  ws.on("message", (data: WebSocketEventData) => {});
+socketServer.on("connection", (socket: WebSocket, req: IncomingMessage) => {
+  const playerID = randomUUID();
+  const roomID = new URL(
+    req.url!,
+    `http://${req.headers.host}`
+  ).searchParams.get("roomID")!;
+  players.set(playerID, <Player>{ roomID, socket });
+  eventHandlers.PlayerJoined({ eventName: "PlayerJoined", roomID, playerID });
+
+  console.log({ roomID });
+
+  socket.on("message", (event: AnyEvent) => {
+    const { eventName } = event;
+    type HandlerType = EventHandler<typeof eventName>;
+    const handleEvent = eventHandlers[eventName] as HandlerType;
+    handleEvent({ ...event, playerID });
+  });
+
+  socket.on("close", () => {
+    eventHandlers.PlayerLeft({ eventName: "PlayerLeft", roomID, playerID });
+  });
 });
 
 httpServer.listen(process.env.PORT ?? 3000, () =>
