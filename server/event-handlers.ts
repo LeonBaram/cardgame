8dafc7c4ba19d2dc8f387d520906a1a3d5c62dd1
LeonBaram <leonbaram@protonmail.com>
2022-07-14 22:19:12 -0400

    Refactor event handlers & impl. PlayerJoined
    - refactor event handlers as separate functions
    - create basic implementation of PlayerJoined handler

--- server/event-handlers.ts
+++ server/event-handlers.ts
@@ -1,155 +1,179 @@
-import {
-  CommonEvent,
-  EventContext,
-  EventData,
-  EventName,
-  GameObjectEvent,
-  GameObjectName,
-  Server,
-} from "../models";
+import { EventContext, EventData, Room } from "../models";
 
-export const eventHandlers: {
-  [ev in EventName]: (ctx: EventContext, data: EventData[ev]) => void;
-} = {
-  PlayerJoined: function (ctx: EventContext, data: CommonEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  PlayerLeft: function (ctx: EventContext, data: CommonEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  NewHost: function (
-    ctx: EventContext,
-    data: CommonEvent & { newHostID: string }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomChangedSize: function (
-    ctx: EventContext,
-    data: CommonEvent & { newRoomSize: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomLocked: function (ctx: EventContext, data: CommonEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomUnlocked: function (ctx: EventContext, data: CommonEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomEnabledPassword: function (
-    ctx: EventContext,
-    data: CommonEvent & { passwordHash: string }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomDisabledPassword: function (ctx: EventContext, data: CommonEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  RoomChangedPassword: function (
-    ctx: EventContext,
-    data: CommonEvent & { passwordHash: string }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  GameObjectDeleted: function (ctx: EventContext, data: GameObjectEvent): void {
-    throw new Error("Function not implemented.");
-  },
-  GameObjectMoved: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { x: number; y: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  GameObjectRotated: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { angle: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  GameObjectFlipped: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { isFaceUp: boolean }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  GameObjectCopied: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { newGameObjectID: string; x: number; y: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  CardCreated: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Card" } & { scryfallID: string }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  DeckCreated: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Deck" } & { scryfallIDs: string[] }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  DeckInsertedCard: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Deck" } & { cardID: string; index: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  DeckRemovedCard: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Deck" } & { index: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  DeckReordered: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Deck" } & { indices: number[] }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  CounterCreated: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Counter" } & { val: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-  CounterUpdated: function (
-    ctx: EventContext,
-    data: CommonEvent & {
-      gameObjectName: GameObjectName;
-      gameObjectID: string;
-    } & { gameObjectName: "Counter" } & { val: number }
-  ): void {
-    throw new Error("Function not implemented.");
-  },
-};
+// Room Events
+export function PlayerJoined(
+  ctx: EventContext,
+  data: EventData<"PlayerJoined">
+): boolean {
+  const { players, rooms, playerID, roomID } = ctx;
+  const player = players.get(playerID)!;
+  const room = rooms.get(roomID);
+
+  // if destination room exists, check if new player is allowed
+  if (room) {
+    // TODO: check if room is full, locked, or password-enabled
+  }
+
+  // leave old room if necessary
+  const prevRoomID = player.roomID;
+  if (rooms.has(prevRoomID)) {
+    PlayerLeft({ ...ctx, roomID: prevRoomID }, data);
+  }
+
+  // create new room if necessary
+  if (!room) {
+    rooms.set(roomID, <Room>{
+      playerIDs: new Set(playerID),
+      hostPlayerID: playerID,
+      gameObjects: new Map(),
+    });
+  }
+
+  player.roomID = roomID;
+
+  return false;
+}
+
+export function PlayerLeft(
+  ctx: EventContext,
+  data: EventData<"PlayerLeft">
+): boolean {
+  return false;
+}
+
+export function NewHost(
+  ctx: EventContext,
+  data: EventData<"NewHost">
+): boolean {
+  return false;
+}
+
+export function RoomChangedSize(
+  ctx: EventContext,
+  data: EventData<"RoomChangedSize">
+): boolean {
+  return false;
+}
+
+export function RoomLocked(
+  ctx: EventContext,
+  data: EventData<"RoomLocked">
+): boolean {
+  return false;
+}
+
+export function RoomUnlocked(
+  ctx: EventContext,
+  data: EventData<"RoomUnlocked">
+): boolean {
+  return false;
+}
+
+export function RoomEnabledPassword(
+  ctx: EventContext,
+  data: EventData<"RoomEnabledPassword">
+): boolean {
+  return false;
+}
+
+export function RoomDisabledPassword(
+  ctx: EventContext,
+  data: EventData<"RoomDisabledPassword">
+): boolean {
+  return false;
+}
+
+export function RoomChangedPassword(
+  ctx: EventContext,
+  data: EventData<"RoomChangedPassword">
+): boolean {
+  return false;
+}
+
+// Game Object Events
+export function GameObjectDeleted(
+  ctx: EventContext,
+  data: EventData<"GameObjectDeleted">
+): boolean {
+  return false;
+}
+
+export function GameObjectMoved(
+  ctx: EventContext,
+  data: EventData<"GameObjectMoved">
+): boolean {
+  return false;
+}
+
+export function GameObjectRotated(
+  ctx: EventContext,
+  data: EventData<"GameObjectRotated">
+): boolean {
+  return false;
+}
+
+export function GameObjectFlipped(
+  ctx: EventContext,
+  data: EventData<"GameObjectFlipped">
+): boolean {
+  return false;
+}
+
+export function GameObjectCopied(
+  ctx: EventContext,
+  data: EventData<"GameObjectCopied">
+): boolean {
+  return false;
+}
+
+// Card Specific Events
+export function CardCreated(
+  ctx: EventContext,
+  data: EventData<"CardCreated">
+): boolean {
+  return false;
+}
+
+// Deck Specific Events
+export function DeckCreated(
+  ctx: EventContext,
+  data: EventData<"DeckCreated">
+): boolean {
+  return false;
+}
+
+export function DeckInsertedCard(
+  ctx: EventContext,
+  data: EventData<"DeckInsertedCard">
+): boolean {
+  return false;
+}
+
+export function DeckRemovedCard(
+  ctx: EventContext,
+  data: EventData<"DeckRemovedCard">
+): boolean {
+  return false;
+}
+
+export function DeckReordered(
+  ctx: EventContext,
+  data: EventData<"DeckReordered">
+): boolean {
+  return false;
+}
+
+// Counter Specific Events
+export function CounterCreated(
+  ctx: EventContext,
+  data: EventData<"CounterCreated">
+): boolean {
+  return false;
+}
+
+export function CounterUpdated(
+  ctx: EventContext,
+  data: EventData<"CounterUpdated">
+): boolean {
+  return false;
+}
