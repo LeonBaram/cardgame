f2ea91ddde441a6bff2be7e1092b99728b491744
LeonBaram <leonbaram@protonmail.com>
2022-07-16 02:31:23 -0400

    Added Room properties size, isLocked, passwordHash

--- models/Room.d.ts
+++ models/Room.d.ts
@@ -4,4 +4,7 @@ export type Room = {
   playerIDs: Set<string>;
   hostPlayerID: string;
   gameObjects: Map<string, Server.GameObject<GameObjectName>>;
+  size: number;
+  isLocked: boolean;
+  passwordHash: string | null;
 };
