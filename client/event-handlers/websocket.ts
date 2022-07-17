17e07530674d2f439a2d3ebf532543cf62946d10
LeonBaram <leonbaram@protonmail.com>
2022-07-16 21:29:21 -0400

    Create separate definition for client event handler context

--- client/event-handlers/websocket.ts
+++ client/event-handlers/websocket.ts
@@ -1,7 +1,13 @@
 import type { Events, EventName } from "../../models";
 
+type Context = {};
+
+interface Handler<E extends EventName> {
+  (ctx: Context, data: Events.Data<E>): boolean;
+}
+
 export const handlers: {
-  [E in EventName]: Events.Handler<E>;
+  [E in EventName]: Handler<E>;
 } = {
   PlayerJoined,
   PlayerLeft,
@@ -24,123 +30,117 @@ export const handlers: {
 };
 
 function PlayerJoined(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"PlayerJoined">
 ): boolean {
   return false;
 }
 
-function PlayerLeft(
-  ctx: Events.Context,
-  data: Events.Data<"PlayerLeft">
-): boolean {
+function PlayerLeft(ctx: Context, data: Events.Data<"PlayerLeft">): boolean {
   return false;
 }
 
-function NewHost(ctx: Events.Context, data: Events.Data<"NewHost">): boolean {
+function NewHost(ctx: Context, data: Events.Data<"NewHost">): boolean {
   return false;
 }
 
 function RoomChangedSize(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"RoomChangedSize">
 ): boolean {
   return false;
 }
 
-function RoomLocked(
-  ctx: Events.Context,
-  data: Events.Data<"RoomLocked">
-): boolean {
+function RoomLocked(ctx: Context, data: Events.Data<"RoomLocked">): boolean {
   return false;
 }
 
 function RoomUnlocked(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"RoomUnlocked">
 ): boolean {
   return false;
 }
 
 function RoomEnabledPassword(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"RoomEnabledPassword">
 ): boolean {
   return false;
 }
 
 function RoomDisabledPassword(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"RoomDisabledPassword">
 ): boolean {
   return false;
 }
 
 function RoomChangedPassword(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"RoomChangedPassword">
 ): boolean {
   return false;
 }
 
 function GameObjectCreated(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"GameObjectCreated">
 ): boolean {
   return false;
 }
 
 function GameObjectDeleted(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"GameObjectDeleted">
 ): boolean {
   return false;
 }
 
 function GameObjectMoved(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"GameObjectMoved">
 ): boolean {
   return false;
 }
 
 function GameObjectRotated(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"GameObjectRotated">
 ): boolean {
   return false;
 }
 
 function GameObjectFlipped(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"GameObjectFlipped">
 ): boolean {
   return false;
 }
 
 function DeckInsertedCard(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"DeckInsertedCard">
 ): boolean {
   return false;
 }
 
 function DeckRemovedCard(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"DeckRemovedCard">
 ): boolean {
   return false;
 }
 
 function DeckRearranged(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"DeckRearranged">
 ): boolean {
   return false;
 }
 
 function CounterUpdated(
-  ctx: Events.Context,
+  ctx: Context,
   data: Events.Data<"CounterUpdated">
 ): boolean {
   return false;
