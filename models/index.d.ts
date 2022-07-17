6a403a219166fd275758bfd2b34ce1c3fa6dbf96
LeonBaram <leonbaram@protonmail.com>
2022-07-16 21:37:39 -0400

    Update models/index exports

--- models/index.d.ts
+++ models/index.d.ts
@@ -1,5 +1,7 @@
+export type { Client } from "./Client";
+export type { Server } from "./Server";
 export type { EventName, Events } from "./Events";
-export type { Client, Server, GameObjectName } from "./GameObjects";
+export type { GameObjectName } from "./GameObjects";
 export type { ScryfallCardData } from "./ScryfallCardData";
 export type { Player } from "./Player";
 export type { Room } from "./Room";
