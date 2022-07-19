40aa93ac31402591dd7f64422901e38a185eb7e4
LeonBaram <leonbaram@protonmail.com>
2022-07-18 20:03:38 -0400

    Refactor scryfall import and implement newCard()

--- client/utils/game-object-constructors.ts
+++ client/utils/game-object-constructors.ts
@@ -1,14 +1,74 @@
-import type { ScryfallCardData } from "../../models";
+import type {
+  Client,
+  GameObjectName,
+  Room,
+  ScryfallCardData,
+} from "../../models";
 
 const scryfallAPI = "https://api.scryfall.com";
 
-export async function fetchScryfallCardData(
-  cardName: string
-): Promise<ScryfallCardData> {
-  const response = await fetch(`${scryfallAPI}/cards/named?fuzzy=${cardName}`);
-  const cardData = await response.json();
-  if (!cardData) {
-    throw `Scryfall has no cards with name similar to ${cardName}`;
+type FetchArgs = { cardName: string } | { scryfallID: string };
+type Data = ScryfallCardData;
+
+export async function fetchScryfallCardData(args: FetchArgs): Promise<Data> {
+  let res: Response;
+  if ("cardName" in args) {
+    res = await fetch(`${scryfallAPI}/cards/named?fuzzy=${args.cardName}`);
+  } else if ("scryfallID" in args) {
+    res = await fetch(`${scryfallAPI}/cards/${args.scryfallID}`);
+  } else {
+    throw "invalid args: must have either cardName or scryfallID";
   }
-  return cardData;
+
+  const data = await res.json();
+  const notFound = {
+    object: "error",
+    code: "not_found",
+    status: 404,
+  };
+
+  for (const key in notFound) {
+    if (key in data && data[key] === notFound[key]) {
+      if ("details" in data) {
+        throw data.details;
+      } else {
+        throw "api error: card not found";
+      }
+    }
+  }
+
+  return data as Data;
+}
+
+type ConstructArgs<G extends GameObjectName = GameObjectName> =
+  Client.GameObjectData<G> & {
+    room: Room<"Client">;
+    scene: Phaser.Scene;
+    spawnX: number;
+    spawnY: number;
+  };
+
+export function newCard(args: ConstructArgs<"Card">): Client.Card {
+  const { room, scene, spawnX, spawnY, data } = args;
+  const { image_uris, id } = data;
+  const sprite = scene.add.image(spawnX, spawnY, id);
+  const uri =
+    image_uris?.png ??
+    image_uris?.large ??
+    image_uris?.normal ??
+    image_uris?.small ??
+    null;
+  if (uri === null) {
+    throw "TODO: load placeholder";
+  } else {
+    scene.load
+      .image(id, data.image_uris!.png!)
+      .once(Phaser.Loader.Events.COMPLETE, () => {
+        sprite.setTexture(id).setScale(0.3).setInteractive();
+        this.input.setDraggable(sprite);
+      })
+      .start();
+  }
+
+  throw "TODO: implement";
 }
