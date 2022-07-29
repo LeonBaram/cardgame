f9bbc0ac464bae039b2c33d98235c679f38cb33e
LeonBaram <leonbaram@protonmail.com>
2022-07-29 09:25:13 -0400

    Simplify card imports; refactor memoization
    Instead of individually memoizing the results of the fetch-by-name &
    fetch-by-ID functions, memoize the results of the "fetch" function
    itself, which both of those functions depend on.

--- client/utils/card-importers.ts
+++ client/utils/card-importers.ts
@@ -11,52 +11,23 @@ type ApiError = {
   details?: string;
 };
 
-const defaultError: ApiError = {
-  object: "error",
-  code: "not_found",
-  status: 404,
+const fetchCache = new LRU_Cache<string, Promise<Response>>(1000);
+
+const memoizedFetch = (url: string): Promise<Response> => {
+  if (!fetchCache.has(url)) {
+    fetchCache.set(url, fetch(url));
+  }
+  return fetchCache.get(url)!;
 };
 
 export type ApiFetcher = (s: string) => Promise<ScryfallCardData | ApiError>;
 
-type Name = string;
-type ID = string;
-const fuzzyNameCache = new LRU<Name, ID | null>(600);
-const cardDataCache = new LRU<ID, ScryfallCardData>(600);
-
 export const scryfallFetchByName: ApiFetcher = async (name) => {
-  if (!fuzzyNameCache.has(name)) {
-    const res = await fetch(`${scryfallAPI}/cards/named?fuzzy=${name}`);
-    const data: ScryfallCardData | ApiError = await res.json();
-
-    if (data.object === "error") {
-      fuzzyNameCache.set(name, null);
-    } else {
-      const { id } = data;
-      fuzzyNameCache.set(name, id);
-      cardDataCache.set(id, data);
-    }
-  }
-
-  const id = fuzzyNameCache.get(name);
-
-  if (id === null) {
-    return defaultError;
-  }
-
-  return scryfallFetchByID(id!);
+  const res = await memoizedFetch(`${scryfallAPI}/cards/named?fuzzy=${name}`);
+  return await res.json();
 };
 
 export const scryfallFetchByID: ApiFetcher = async (id) => {
-  if (!cardDataCache.has(id)) {
-    const res = await fetch(`${scryfallAPI}/cards/${id}`);
-    const data: ScryfallCardData | ApiError = await res.json();
-
-    if (data.object !== "error") {
-      const { id } = data;
-      cardDataCache.set(id, data);
-    }
-  }
-
-  return cardDataCache.get(id)!;
+  const res = await memoizedFetch(`${scryfallAPI}/cards/${id}`);
+  return await res.json();
 };
